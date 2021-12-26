6a67ec0387ff0c5cf9b23860c88d4f2321db33ba
Javier Torné <javietorne88@gmail.com>
2021-12-26 12:59:22 +0100

    Initial Commit

---
+++ MagicJTH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicJTH
+//
+//  Created by Javier Torné Hernández on 26/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
