1059cba6efcb9569c7411db696e3d0b71d9ae117
Javier Torné <javietorne88@gmail.com>
2021-12-27 09:45:44 +0100

    Primeros pasos en vista de bienvenida

--- MagicJTH/Screens/Welcome/WelcomeView.swift
+++ MagicJTH/Screens/Welcome/WelcomeView.swift
@@ -5,12 +5,36 @@
 //  Created by Javier Torné Hernández on 27/12/21.
 //
 
+import UIKit
+
 /// Vista de la pantalla de bienvenida
 class WelcomeView: ViewForViewController {
     
     // MARK: - Propiedades
     
     private var hierarchyNotReady = true
+    
+    // MARK: - Vistas
+    
+    private lazy var stackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [nameSurnameLabel, emailLabel])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        //stack.spacing = Measurement.halfMargin
+        return stack
+    }()
+    
+    private let nameSurnameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Javier Torné Hernández"
+        return label
+    }()
+    
+    private let emailLabel: UILabel = {
+        let label = UILabel()
+        label.text = "javiertorne88@gmail.com"
+        return label
+    }()
     
     // MARK: - Métodos
     
@@ -29,15 +53,20 @@ class WelcomeView: ViewForViewController {
     }
     
     func styleView() {
-        backgroundColor = .brown
+        backgroundColor = .systemBackground
     }
     
     func constructHierachy() {
-        
+        addSubview(stackView)
     }
     
     func anchorViews() {
-        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
     }
     
 }
