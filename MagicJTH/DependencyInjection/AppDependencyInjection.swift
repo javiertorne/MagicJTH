95c69c20e22b1fa1af489276bfcd6e373ca6e2fd
Javier Torné <javietorne88@gmail.com>
2021-12-27 09:11:32 +0100

    Creación del contenedor principal de inyección de dependencias

---
+++ MagicJTH/DependencyInjection/AppDependencyInjection.swift
@@ -0,0 +1,26 @@
+//
+//  AppDependencyInjection.swift
+//  MagicJTH
+//
+//  Created by Javier Torné Hernández on 27/12/21.
+//
+
+/// Contenedor principal de inyección de dependencias.
+class AppDependencyInjection {
+    
+    // MARK: - Métodos
+    
+    // MARK: Main
+    
+    /// Creación del view controller que actúa como contenedor principal de vistas.
+    func makeMainContainerViewController() -> MainContainerViewController {
+        let mainContainerViewModel = makeMainContainerViewModel()
+        return MainContainerViewController(viewModel: mainContainerViewModel)
+    }
+    
+    /// Creación de un view model para el view controller que actúa como contenedor principal de vistas.
+    private func makeMainContainerViewModel() -> MainContainerViewModel {
+        MainContainerViewModel()
+    }
+    
+}
